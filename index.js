699b4f4b08785cd46adfdf1b5dfc74664d496440
abigail <abigail.wamai@student.moringaschool.com>
2023-06-15 20:52:31 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+function cloneObject(object) {
+    return Object.assign({}, object);
+  }
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = cloneObject(employee);
+    newEmployee[key] = value;
+    return newEmployee;
+  }
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = cloneObject(employee);
+    delete newEmployee[key];
+    return newEmployee;
+  }
